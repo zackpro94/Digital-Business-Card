35db1eeaef04bea786cc8528e14a5a7419e2b074
zackpro94 <zekarias94@gmail.com>
2025-03-23 00:27:02 +0300

    login page added

--- digital_business_card/settings.py
+++ digital_business_card/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'digital_business_card.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
