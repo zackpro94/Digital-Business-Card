44742c2c215a07ac58290f8d3bcec6da73b63498
zackpro94 <zekarias94@gmail.com>
2025-03-22 23:51:12 +0300

    setting modified

--- digital_business_card/settings.py
+++ digital_business_card/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s1w55#8ama35^5_98a4z3yf*#b+g2sacg@ok)h4%za*=g2621o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
