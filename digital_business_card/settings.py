0a1e97a0afea03121d0684dac93e353969f8fd16
zackpro94 <zekarias94@gmail.com>
2025-03-22 23:47:43 +0300

    setting modified

--- digital_business_card/settings.py
+++ digital_business_card/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS= [""]
+CSRF_TRUSTED_ORIGINS= ["https://digital-business-card-production.up.railway.app/"]
 
 # Application definition
 
