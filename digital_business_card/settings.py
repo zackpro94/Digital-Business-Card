882a7bcce7299240a746f7c5091652dac197b2e8
zackpro94 <zekarias94@gmail.com>
2025-03-29 16:53:18 +0300

    test

--- digital_business_card/settings.py
+++ digital_business_card/settings.py
@@ -81,22 +81,15 @@ WSGI_APPLICATION = 'digital_business_card.wsgi.application'
 # settings.py
 import dj_database_url
 
-DATABASE_URL = os.getenv('postgresql://postgres:vzsVRGhxDYCYDvtGbKsHyxBGnljscban@postgres.railway.internal:5432/railway', 'sqlite:///db.sqlite3')
+DATABASE_URL = os.getenv('postgresql://postgres:vzsVRGhxDYCYDvtGbKsHyxBGnljscban@postgres.railway.internal:5432/railway')
 
-if DATABASE_URL.startswith('postgresql://'):
-    DATABASES = {
-        'default': dj_database_url.config(
-            conn_max_age=600,
-            ssl_require=True  # Only for PostgreSQL
-        )
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=True  # Required for Railway PostgreSQL
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
